a00a701c941d416b7c768fee94b9c0f89f6ebcc8
Pradhumn Siorthiya <sirothiyapradhumn@gmail.com>
2022-02-04 20:41:06 +0530

    Initial Programs

---
+++ LEVEL1/GettingStarted/GradingSystem.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+       Scanner scn  = new Scanner(System.in);
+       int n = scn.nextInt();
+       boolean m = scn.nextBoolean();
+       double o = scn.nextDouble();
+       String s = scn.next();
+
+       System.out.println(n);
+       System.out.println(m);
+       System.out.println(o);
+       System.out.println(s);
+
+    }
+}
