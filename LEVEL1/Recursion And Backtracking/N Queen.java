024a018de3c868bd41492388364e48db36e0744f
Pradhumn Siorthiya <sirothiyapradhumn@gmail.com>
2022-04-01 19:13:18 +0530

    N Queen

---
+++ LEVEL1/Recursion And Backtracking/N Queen.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        boolean [][] chess = new boolean[n][n];
+        printNQueens(chess, "", 0);
+    }
+
+    public static void printNQueens(boolean[][] chess, String asf, int row) {
+        //base case
+        if(row == chess.length){
+            System.out.println(asf+".");
+            return;
+        }
+
+        for(int col = 0; col<chess.length; col++){
+            if(isSafe(chess, row, col)==true){
+                chess[row][col] = true;
+                printNQueens(chess, asf+row+"-"+col+", ", row+1);
+                chess[row][col] = false;
+            }
+        }
+    }
+
+    public static boolean isSafe(boolean[][] chess, int row, int col){
+        //diagonal d1
+        for(int i=row-1, j=col+1; i>=0 && j<chess.length; i--, j++){
+            if(chess[i][j]==true){
+                return false;
+            }
+        }
+
+        //diagonal d2
+        for(int i =row-1; i>=0; i--){
+            if(chess[i][col] == true){
+                return false;
+            }
+        }
+
+        //diagonal d3
+        for(int i=row-1, j=col-1; i>=0 && j>=0; i--, j--){
+            if(chess[i][j] == true){
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
