65c84dcf85861f19baa6fe8306dbbf469301e961
Pradhumn Siorthiya <sirothiyapradhumn@gmail.com>
2022-04-24 08:44:07 +0530

    Rotate by 90 degree v1.0

--- LEVEL1/2D Array/Roatate by 90.java
+++ LEVEL1/2D Array/Roatate by 90.java
@@ -4,42 +4,55 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) throws Exception {
+        // write your code here
         Scanner scn = new Scanner(System.in);
-        int n = scn.nextInt();
-        int[][] arr = new int[n][n];
+        int n =scn.nextInt();
+        int arr[][] = new int[n][n];
 
         for(int i = 0; i<n; i++){
-            for(int j = 0; j<n; j++){
+            for(int j = 0; j <n; j++){
                 arr[i][j] = scn.nextInt();
             }
         }
 
         // 1st step -> transposing the matrix
-        for(int i = 0; i<arr.length; i++){
-            for(int j = i+1; j<arr.length; j++){
-                int temp = arr[i][j];
-                arr[i][j] = arr[j][i];
-                arr[j][i] = temp;
+        transpose(arr);
+        // 2nd step -> reverse Single row
+        rotateSingleRow(arr);
+        // 3rd display
+        display(arr);
+        
+    }
+
+    public static void transpose(int arr[][]){
+
+        for(int i = 0; i<arr.length-1; i++){
+            for(int j = i+1; j<arr[0].length; j++){
+                swap(arr, i , j);
             }
         }
+    }
 
-        // 2nd step -> reverse row
+    public static void rotateSingleRow(int arr[][]){
+        for(int i = 0; i<arr.length; i++){
+            int left = 0;
+            int right = arr[0].length -1;
 
-        for(int row = 0; row<n; row++){
-            int left  = 0;
-            int right = n-1;
-            while(left < right){
-                int temp  = arr[row][left];
-                arr[row][left] = arr[row][right];
-                arr[row][right] = temp;
+            while(left<right){
+                int temp = arr[i][left];
+                arr[i][left] = arr[i][right];
+                arr[i][right] = temp;
 
                 left++;
                 right--;
             }
-
         }
+    } 
 
-        display(arr);
+    public static void swap(int arr[][] , int i, int j){
+        int temp = arr[i][j];
+        arr[i][j] = arr[j][i];
+        arr[j][i] = temp;
     }
 
     public static void display(int[][] arr){
