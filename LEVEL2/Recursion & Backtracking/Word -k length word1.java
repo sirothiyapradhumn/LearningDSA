b94457f343abde48d00023bf6bdcc860483aed89
Pradhumn Siorthiya <sirothiyapradhumn@gmail.com>
2022-07-27 14:38:26 +0530

    word-k length word 1 permutaion derived

---
+++ LEVEL2/Recursion & Backtracking/Word -k length word1.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+ 
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    String str = br.readLine();
+    int k = Integer.parseInt(br.readLine());
+
+    HashSet<Character> unique = new HashSet<>();
+    String ustr = "";
+    for (char ch : str.toCharArray()) {
+      if (unique.contains(ch) == false) {
+        unique.add(ch);
+        ustr += ch;
+      }
+    }
+
+    
+
+    word1(ustr, 0, new Character[k], 0, k);
+  }
+
+  public static void word1(String ustr, int cc, Character[] spots, int ssf, int ts) {
+    
+    if(cc == ustr.length()){
+      if(ssf == ts){
+        for(int i =0; i<spots.length; i++){
+          System.out.print(spots[i]);
+        }
+        System.out.println();
+      }
+      //System.out.println();
+      return;
+
+    }
+
+    //yess call - multiple
+    for(int i = 0; i<spots.length; i++){
+      if(spots[i] == null){
+        spots[i] = ustr.charAt(cc);
+        word1(ustr, cc+1, spots, ssf+1, ts);
+        spots[i] = null;
+      }
+    }
+
+    // no call
+    word1(ustr, cc+1, spots, ssf, ts);
+  }
+}
