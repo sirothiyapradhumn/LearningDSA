f063f762ef690d462f38c51a32583bbb214f2d06
Pradhumn Siorthiya <sirothiyapradhumn@gmail.com>
2022-08-23 19:19:11 +0530

    Friend Pairing Problem memo, Tabulation code DP

---
+++ LEVEL2/DP/DP-leetcode/Friends Pairing Problem(GFG).java
@@ -0,0 +1,33 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+import java.io.*;
+import java.util.*;
+class GfG
+{
+    public static void main(String args[])
+        {
+            Scanner sc = new Scanner(System.in);
+            int t = sc.nextInt();
+            while(t-->0)
+                {
+                    int n = sc.nextInt();
+                    Solution ob = new Solution();
+                    System.out.println(ob.countFriendsPairings(n));
+                }
+        }
+}    
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public long countFriendsPairings(int n) 
+    { 
+       //code here
+    }
+}    
+ 
