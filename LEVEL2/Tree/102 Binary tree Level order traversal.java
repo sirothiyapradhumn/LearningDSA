500c409d6487a8fa6d8e690ca282ac1c9ff15dcf
Pradhumn Siorthiya <sirothiyapradhumn@gmail.com>
2022-09-21 21:45:31 +0530

    Level order traverssal

---
+++ LEVEL2/Tree/102 Binary tree Level order traversal.java
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> ans = new ArrayList<>();
+        if(root == null) return ans;
+        
+        Queue<TreeNode> q = new LinkedList<>();  
+        q.add(root);
+        
+        while(q.size()!=0){
+            int qsize = q.size();
+            List<Integer> ansList = new LinkedList<>();
+            
+            while(qsize -- !=0){
+                TreeNode node = q.remove();
+                ansList.add(node.val);
+                
+                if(node.left != null){
+                    q.add(node.left);
+                }
+                
+                if(node.right != null){
+                    q.add(node.right);
+                }
+            }
+            
+            ans.add(ansList);
+        }
+        
+        return ans;
+    }
+}
